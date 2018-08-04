22d3e6dd4c548f8061efdcb492f60c4a099dd9c9
stephanie <b06303077@ntu.edu.tw>
2018-08-05 01:22:44 +0800

    change

--- week6/FinalProject.R
+++ week6/FinalProject.R
@@ -21,3 +21,30 @@ library(igraph)
 data <- fromJSON("../data/train.json")
 data <- as.data.frame(data)
 head(flatten(data))
+train <- fromJSON("../data/train.json", flatten = TRUE)
+test <- fromJSON("../data/test.json", flatten = TRUE)
+
+ingredientscombine <- function(s)
+{
+  a <- unlist(s)
+       return(paste0(a, collapse = '',sep=' '))
+}
+train$ingredients <- sapply(train$ingredients,ingredientscombine)
+train <- train %>%rename(text = ingredients)
+test$ingredients <- sapply(test$ingredients,ingredientscombine)
+
+test <- test %>%rename(text = ingredients)
+
+wordcloudcuisine= function(train,cuisineName)
+{   train %>%
+    unnest_tokens(word, text) %>%
+    filter(!word %in% stop_words$word) %>%
+    filter(!word %in% most_common_words$word) %>%
+    filter(cuisine == cuisineName) %>%
+    count(word,sort = TRUE) %>%
+    ungroup()  %>%
+    head(30) 
+  with(wordcloud(word, n, max.words = 30,colors=brewer.pal(8, "Dark2")))
+}
+wordcloudcuisine(train,"italian")
+
--- week6/cuisine shiny/app.R
+++ week6/cuisine shiny/app.R
@@ -8,24 +8,120 @@
 #
 
 library(shiny)
-
+library(wordcloud)
+library(RColorBrewer)
 # Define UI for application that draws a histogram
-ui <- navbarPage("Worldwide Cuisine",tabPanel("前言"),navbarMenu("世界",tabPanel("world",h1(fluidPage()))),navbarMenu("異國料理",tabPanel("文字雲")))
+cuisine<<-list("italian","southern-us","mexican","chinese","french")
+ui <- navbarPage("Worldwide Cuisine",tabPanel("前言"),
+                 navbarMenu("世界",tabPanel("world",h1(fluidPage()))),
+                 navbarMenu("異國料理",tabPanel("長條圖",h1(fluidPage(
+                                                                       titlePanel("Barplot"),
+                                                                       sidebarLayout(sidebarPanel(selectInput("cuisine", "選擇國家",choices=colnames(cuisine)),
+                                                                       hr()),
+                                                                       mainPanel( plotOutput("cbarplot"))))))
+                                       #tabPanel("文字雲",h1(fluidPage(titlePanel("Word Cloud"),
+                                                                      #sidebarLayout(sidebarPanel(selectInput("cuisine", "選擇國家", choices = cuisine),
+                                                                      #actionButton("update","change"),
+                                                                      #hr(),
+                                                                      #sliderInput("freq", "Minimum Frequency:",min = 1,  max = 50, value = 15),
+                                                                      #sliderInput("max","Maximum Number of Words:",min = 1,  max = 300,  value = 100)),
+                                                                      #mainPanel(plotOutput("wordcloudcuisine")))
+                                                                      
+                 ))
 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+  library(jsonlite)
+  library(tidyverse)
+  library(tidytext)
+  library(scales)
+  library(stringr)
+  library(wordcloud)
+  library(treemap)
+  library(text2vec)
+  library(glmnet)
+  library(igraph)
+  library(ggraph)
+  library(knitr)
+  
+  rm(list=ls())
+  
+  fillColor = "#FFA07A"
+  fillColor2 = "#F1C40F"
+  fillColorBlue = "#AED6F1"
+  
+  train <- fromJSON("../data/train.json", flatten = TRUE)
+  test <- fromJSON("../data/test.json", flatten = TRUE)
+  train2<-train
+  
+ingredientscombine <- function(s)
+  {
+    a <- unlist(s)
+    return(paste0(a, collapse = '',sep=' '))
+  }
+train$ingredients <- sapply(train$ingredients,ingredientscombine)
+train <- train %>%
+         rename(text = ingredients)
+test$ingredients <- sapply(test$ingredients,ingredientscombine)
+test <- test %>%
+        rename(text = ingredients)
 
+most_common_words <- train %>%
+                     unnest_tokens(word, text) %>%
+                     filter(!word %in% stop_words$word) %>%
+                     count(word,sort = TRUE) %>%
+                     ungroup() %>%
+                     mutate(word = factor(word, levels = rev(unique(word)))) %>%
+                     head(20)
+most_common_ingredients <- train%>% 
+                           mutate(ingredients = str_split(ingredients, pattern = ",")) %>% 
+                           unnest(ingredients) %>% 
+                           mutate(ingredients = gsub(ingredients, pattern = 'c\\(', replacement = "")) %>%
+                           mutate(ingredients = gsub(ingredients, pattern = '"', replacement = "")) %>%
+                           mutate(ingredients = trimws(ingredients)) %>%
+                           group_by(ingredients) %>%
+                           summarise(Count = n()) %>%
+                           arrange(desc(Count)) %>%
+                           ungroup() %>%
+                           head(10)
+#barplot
+barplotcuisine= function(train,cuisineName,titleName,fillColorName)
+{
+  train %>% 
+  filter(cuisine == cuisineName) %>%
+  mutate(ingredients = str_split(ingredients, pattern = ",")) %>% 
+  unnest(ingredients) %>% 
+  mutate(ingredients = gsub(ingredients, pattern = 'c\\(', replacement = "")) %>%
+  mutate(ingredients = gsub(ingredients, pattern = '"', replacement = "")) %>%
+  mutate(ingredients = trimws(ingredients)) %>%
+  filter(!ingredients %in% most_common_ingredients$ingredients) %>%
+  group_by(ingredients) %>%
+  summarise(Count = n()) %>%
+  arrange(desc(Count)) %>%
+  ungroup() %>%
+  mutate(ingredients = reorder(ingredients,Count)) %>%
+  head(10) %>%
+  ggplot(aes(x = ingredients,y = Count)) +
+  geom_bar(stat='identity',fill= fillColor2) +
+  geom_text(aes(x = ingredients, y = .01, label = paste0("( ",Count," )",sep="")),hjust=0, vjust=.5, size = 4, colour = 'black',fontface = 'bold') +
+  labs(x = 'ingredients', y = 'Count', title = titleName) +
+  coord_flip() 
+output$cbarplot <- barplotcuisine
+#wordcloud
+#wordcloudcuisine= function(train,cuisine)
+  #{  train %>%
+      #unnest_tokens(word, text) %>%
+      #filter(!word %in% stop_words$word) %>%
+      #filter(!word %in% most_common_words$word) %>%
+      #filter(cuisine == cuisineName) %>%
+      #count(word,sort = TRUE) %>%
+      #ungroup()  %>%
+      #head(30) 
+  #with(wordcloud(word, n, max.words = 30,colors=brewer.pal(8, "Dark2")))
+  
+}
+}
 # Run the application 
 shinyApp(ui = ui, server = server)
 
